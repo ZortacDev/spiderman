d2cb216b8ef7c5606bf047dc86f2a930a9d15213
Moritz Kuntze <mfk@zortac.net>
2023-01-16 23:51:27 +0100

    Added documentation

--- README.md
+++ README.md
@@ -1,10 +1,43 @@
 # spiderman: The weaving project manager
-`spiderman` is a utility for managing digital projects, be it software, electronics, art or anything else.
-It is designed to automate the organization of your projects by associating each project with a set of _tags_ and then generating and maintaining a set of _views_
-based on these tags.
 
-Each view is a directory structure with the deepest level being symbolic links to the actual project directories. 
-Views are defines through _schemas_ which define how the directory hierarchy should be constructed based on project tags and fixed directory elements.
+Spiderman manages your projects by associating each project with a set of _tags_ and dynamically generating 
+set of _views_ based on these tags.
+
+All projects live within a spiderman project root. All subcommands (except **init** which creates a new root) 
+operate with respect to the _current_ project root. The project root is either the next upstream 
+directory (from the current working directory) that qualifies as a project root, or the default project root, 
+which can be set in `~/.config/spiderman/config.toml`.
+
+A spiderman project root contains a `.spiderman` directory, which, in turn, contains the `raw` directory, 
+holding all the projects managed by spiderman, and the `schema.toml` configuration file, describing how 
+spiderman should build view trees based on tags.
+
+The `schema.toml` file contains a list of schemas and a set of default tag values to use when a 
+project does not have a tag, but that tag is used in a schema. Schemas consist of `/` separated components 
+that are either plain strings, in which case they will be used in the view tree verbatim, or strings contained 
+in curly braces (`{` and `}`) in which case the string is interpreted as the name of a tag and substituted 
+with the appropriate tag value when building the view tree.
+
+A project's tags are specified in its `spiderman.tags` file, which can be edited using the **tags** subcommand. 
+Each line in this file consists of colon (`:`) separated values. The first of these is the name of the tag, while the 
+later ones are values for that tag. A project may have multiple values for one tag.
 
 ## Example
-_TODO_
+`schema.toml`:
+
+```toml
+schemas = [
+    "by-organization/{organization}/{type}",
+    "by-type/{type}/{organization}"
+]
+
+[default_tag_values]
+organization = "Personal"
+type = "Random"
+```
+
+`spiderman.tags`:
+```
+organization:Uni
+type:Software:Writing
+```
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,30 @@ use crate::file_utils::open_in_editor;
 use crate::project::Project;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version)]
+/// The Weaving Project Manager
+///
+/// Spiderman manages your projects by associating each project with a set of _tags_ and dynamically generating
+/// set of _views_ based on these tags.
+///
+/// All projects live within a spiderman project root. All subcommands (except **init** which creates a new root)
+/// operate with respect to the _current_ project root. The project root is either the next upstream
+/// directory (from the current working directory) that qualifies as a project root, or the default project root,
+/// which can be set in `~/.config/spiderman/config.toml`.
+///
+/// A spiderman project root contains a `.spiderman` directory, which, in turn, contains the `raw` directory,
+/// holding all the projects managed by spiderman, and the `schema.toml` configuration file, describing how
+/// spiderman should build view trees based on tags.
+///
+/// The `schema.toml` file contains a list of schemas and a set of default tag values to use when a
+/// project does not have a tag, but that tag is used in a schema. Schemas consist of `/` separated components
+/// that are either plain strings, in which case they will be used in the view tree verbatim, or strings contained
+/// in curly braces (`{` and `}`) in which case the string is interpreted as the name of a tag and substituted
+/// with the appropriate tag value when building the view tree.
+///
+/// A project's tags are specified in its `spiderman.tags` file, which can be edited using the **tags** subcommand.
+/// Each line in this file consists of colon (`:`) separated values. The first of these is the name of the tag, while the
+/// later ones are values for that tag. A project may have multiple values for one tag.
 struct Cli {
     #[command(subcommand)]
     command: Commands
@@ -26,16 +49,24 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Re-creates the view tree
     Weave,
+    /// Initializes a new spiderman project root
     Init {
+        /// Directory to use instead of the current directory as the project root
         dir: Option<PathBuf>
     },
+    /// Creates a new project
     New {
+        /// Name of the project to be created
         name: String
     },
+    /// Moves an existing project into the current spiderman project root
     Move {
+        /// Path to the project to be moved, must be a directory
         source: PathBuf
     },
+    /// Edit tags of the current project (the current working directory must be a project directory)
     Tags
 }
 
